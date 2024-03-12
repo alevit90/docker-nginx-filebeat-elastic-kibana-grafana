e838be576ca8095f193ba95238ac3c801a745ab5
alevit <147342836+alevit90@users.noreply.github.com>
2024-03-12 22:18:55 +0100

    Update script_di_creazione_modello.sh

--- curl/script_di_creazione_modello.sh
+++ curl/script_di_creazione_modello.sh
@@ -13,9 +13,9 @@ curl -k http://nginx:80
 curl -k http://nginx:80
 curl -k http://nginx:80
 
-# Waiting for 200 seconds to wait for the kibana container to be operational
+# Waiting for 130 seconds to wait for the kibana container to be operational
 
-sleep 240
+sleep 130
 
 # Running the curl to create the index pattern
 
